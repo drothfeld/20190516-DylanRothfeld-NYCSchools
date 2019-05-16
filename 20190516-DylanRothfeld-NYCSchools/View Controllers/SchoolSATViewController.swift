0eb4c099685191d2a4dc9b22788543ceeb22a9a8
Dylan Rothfeld <drothfel@gmail.com>
2019-05-16 10:55:43 -0700

    SchoolSAT storyboard with controller outlets and segues

---
+++ 20190516-DylanRothfeld-NYCSchools/View Controllers/SchoolSATViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SchoolSATViewController.swift
+//  20190516-DylanRothfeld-NYCSchools
+//
+//  Created by Dylan Rothfeld on 5/16/19.
+//  Copyright © 2019 Dylan Rothfeld. All rights reserved.
+//
+
+import UIKit
+
+class SchoolSATViewController: UIViewController {
+    // Storyboard Outlets
+    @IBOutlet weak var ControllerTitleLabel: UILabel!
+    @IBOutlet weak var AverageSATReadingScoreLabel: UILabel!
+    @IBOutlet weak var AverageSATMathScoreLabel: UILabel!
+    @IBOutlet weak var AverageSATWritingScoreLabel: UILabel!
+    
+    // Controller Values
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
