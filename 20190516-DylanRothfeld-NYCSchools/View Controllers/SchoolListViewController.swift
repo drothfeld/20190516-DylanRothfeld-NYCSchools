ce393d37dddedfacb1f6f296f376ea5584f458f8
Dylan Rothfeld <drothfel@gmail.com>
2019-05-16 10:46:38 -0700

    bulk of SchoolListViewController logic with api call error handling

--- 20190516-DylanRothfeld-NYCSchools/View Controllers/SchoolListViewController.swift
+++ 20190516-DylanRothfeld-NYCSchools/View Controllers/SchoolListViewController.swift
@@ -13,21 +13,76 @@ class SchoolListViewController: UIViewController, UITableViewDataSource, UITable
     @IBOutlet weak var schoolListTableView: UITableView!
     @IBOutlet weak var searchBar: UISearchBar!
     
+    // Controller Values
+    let apiService = APIService()
+    var isSearching = false
+    var errorOccurred = true
+    var schoolList: [NYCSchool] = []
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        refreshTable()
     }
     
+    // Refreshes the table view
+    func refreshTable() {
+        // Wait for async api call to finish retrieving school data
+        self.showSpinner(onView: self.view)
+        apiService.getNYCSchoolData( completion: { results in
+            switch results {
+                
+            // Successful API call
+            case .success(let schools):
+                self.schoolList = schools
+                // Only display filtered schools if search bar is active
+                if (self.isSearching) {
+                    self.schoolList = self.schoolList.filter({
+                        ($0.name.lowercased().contains(self.searchBar.text!.lowercased())) ||
+                            ($0.location.lowercased().contains(self.searchBar.text!.lowercased()))
+                    })
+                }
+                self.schoolListTableView.reloadData()
+                
+            // An error occurred during API call
+            case .failure(let error):
+                self.showUnableToFetchDataAlert(error: error)
+            }
+            self.removeSpinner()
+        })
+    }
+    
+    // Creates UI alert if there was an issue fetching the data
+    func showUnableToFetchDataAlert(error: Error) {
+        let alert = UIAlertController(title: "An Error Occurred", message: error.localizedDescription, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Retry", style: .default, handler: { action in
+            self.refreshTable()
+        }))
+        self.present(alert, animated: true)
+    }
+    
+    // Returns integer value of entry count in table view
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return schoolList.count
+    }
+    
+    // Returns cell for a single school in the table view
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "schoolCell", for: indexPath)
         
+        let schoolName = schoolList[indexPath.item].name
+        let schoolLocation = schoolList[indexPath.item].location
+        
+        cell.textLabel?.text = schoolName
+        cell.detailTextLabel?.text = schoolLocation
+        
         return cell
     }
-
+    
+    // The searchBar search button is pressed
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        isSearching = (searchBar.text == nil || searchBar.text == "") ?false : true
+        refreshTable()
+        searchBar.endEditing(true)
+    }
 }
-
