41dc29852bd0f6d913fe1a81de2bcb29f3815428
Dylan Rothfeld <drothfel@gmail.com>
2019-05-16 11:25:33 -0700

    populating SchoolSAT view with API data result

--- 20190516-DylanRothfeld-NYCSchools/View Controllers/SchoolSATViewController.swift
+++ 20190516-DylanRothfeld-NYCSchools/View Controllers/SchoolSATViewController.swift
@@ -23,5 +23,36 @@ class SchoolSATViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        fetchNYCSchoolSATData()
+    }
+    
+    // Fetches and displays selected NYCSchool's average SAT scores from API call
+    func fetchNYCSchoolSATData() {
+        self.ControllerTitleLabel.text = schoolName + "\nAverage SAT Scores"
+        self.ControllerTitleLabel.adjustsFontSizeToFitWidth = true
+        
+        // Wait for async api call to finish retrieving school data
+        self.showSpinner(onView: self.view)
+        apiService.getNYCSchoolSATData(schoolID: schoolID, completion: { results in
+            switch results {
+                
+            // Successful API call
+            case .success(let school):
+                self.schoolSAT = school
+                
+                self.AverageSATReadingScoreLabel.text = self.schoolSAT.averageSATScoreReading
+                self.AverageSATMathScoreLabel.text = self.schoolSAT.averageSATScoreMath
+                self.AverageSATWritingScoreLabel.text = self.schoolSAT.averageSATScoreWriting
+                
+            // An error occurred during API call
+            case .failure(let error):
+                let alert = UIAlertController(title: "An Error Occurred", message: error.localizedDescription, preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Retry", style: .default, handler: { action in
+                    self.fetchNYCSchoolSATData()
+                }))
+                self.present(alert, animated: true)
+            }
+            self.removeSpinner()
+        })
     }
 }
